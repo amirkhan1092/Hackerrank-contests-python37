5fbea05735ae91fac93fa21db61e6881f60f6325
amirkhan1092 <amirkhan1092@gmail.com>
2019-02-26 14:49:54 +0530

    valid parenthesis length

---
+++ parenthesis_string_validation.py
@@ -0,0 +1,28 @@
+def findMaxLen(string):
+    n = len(string)
+    stk = []
+    stk.append(-1)
+
+    result = 0
+
+
+    for i in range(n):
+
+        # If opening bracket, push index of it
+        if string[i] == '(':
+            stk.append(i)
+
+        else:  # If closing bracket, i.e., str[i] = ')'
+
+            stk.pop()
+
+            if len(stk) != 0:
+                result = max(result, i - stk[len(stk) - 1])
+
+            else:
+                stk.append(i)
+
+    return result
+k=input('enter the string ')
+
+print(findMaxLen(k))
