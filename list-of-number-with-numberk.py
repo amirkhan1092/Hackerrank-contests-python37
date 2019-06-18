e44e7987b789183fd68b19b18a0b2157a200c0e4
amirkhan1092 <amirkhan1092@gmail.com>
2019-06-18 22:41:16 +0530

    list elements sum equal to the number

---
+++ list-of-number-with-numberk.py
@@ -0,0 +1,24 @@
+# Good morning! Here's your coding interview problem for today.
+#
+# This problem was recently asked by Google.
+#
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+#
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+#
+# Bonus: Can you do this in one pass?
+
+
+a = eval(input('enter the list of numbers '))
+k = eval(input('enter the number '))
+
+flag = False
+for i in range(len(a)):
+    for p in range(len(a)):
+        if a[i] + a[p] == k and i != p:
+            flag = True
+            break
+    if flag:
+        break
+
+print(flag)
