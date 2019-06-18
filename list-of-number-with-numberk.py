c3f562cf87af067aece9871b3baf2938d9496661
amirkhan1092 <amirkhan1092@gmail.com>
2019-06-18 23:14:50 +0530

    list elements sum equal to the number updated with any combination

--- list-of-number-with-numberk.py
+++ list-of-number-with-numberk.py
@@ -8,17 +8,32 @@
 #
 # Bonus: Can you do this in one pass?
 
+import itertools
 
 a = eval(input('enter the list of numbers '))
 k = eval(input('enter the number '))
 
 flag = False
-for i in range(len(a)):
-    for p in range(len(a)):
-        if a[i] + a[p] == k and i != p:
-            flag = True
+
+for i in range(1,len(a)):
+        h = itertools.combinations(a,i)
+        for m in h:
+            if sum(m) == k:
+                flag = True
+                break
+
+        if flag:
             break
-    if flag:
-        break
 
 print(flag)
+
+#
+# for i in range(len(a)):
+#     for p in range(len(a)):
+#         if a[i] + a[p] == k and i != p:
+#             flag = True
+#             break
+#     if flag:
+#         break
+#
+# print(flag)
