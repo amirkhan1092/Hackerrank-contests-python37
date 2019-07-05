7299c6cd08192a8796595c35fe5b58203488a814
amirkhan1092 <amirkhan1092@gmail.com>
2019-07-06 01:56:58 +0530

    fibonacci with recursion

--- fibonacci series.py
+++ fibonacci series.py
@@ -1,15 +1,39 @@
 
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+
+        return fib(n-1) + fib(n-2)
+
+# print(fib(0))
+
+
 num = int(input('enter the length of fibonacci series '))
+for i in range(num):
+    f = fib(i)
+    print(f)
 
-a = 0
-b = 1
-lst = []
-lst.extend((a,b))
-for i in range(num-2):
-    temp = a + b
-    lst.append(temp)
-    a = b
-    b = temp
 
-print(lst)
+
+
+
+
+
+
+#
+#
+# num = int(input('enter the length of fibonacci series '))
+#
+# a = 0
+# b = 1
+# lst = []
+# lst.extend((a,b))
+# for i in range(num-2):
+#     temp = a + b
+#     lst.append(temp)
+#     a = b
+#     b = temp
+#
+# print(lst)
 
